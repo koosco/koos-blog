3a5a09c36560369cc740b9e386849e154acf7511
koosco <rnxogud123@naver.com>
2024-03-11 22:22:39 +0900

    initial commit

---
+++ src/test/java/koos/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package koos.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
